c8e8ed07c50b3eaff1a2c01e70d9b3aac2090dcd
h4rdL1nk <imthedanger@protonmail.com>
2018-06-04 15:32:02 +0200

    Added comments on vars.tf

--- environments/ost-test/vars.tf
+++ environments/ost-test/vars.tf
@@ -1,10 +1,13 @@
+//Empty password variable requested as user input
 variable "ost-password" {}
 
+//Environment needed for estrablishing platform size
 variable "env" {
   type    = "string"
   default = "production"
 }
 
+//Docker instances parameters
 variable "docker-pool-instances" {
   type = "map"
 
